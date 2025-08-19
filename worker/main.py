03a29c364ca9f031eec81d52f522192bbe9edfb1
chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>
2025-08-19 22:30:10 +0530

    prod
    Signed-off-by: chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>

--- worker/main.py
+++ worker/main.py
@@ -20,4 +20,4 @@ async def root():
 
 if __name__ == "__main__":
     port = int(os.getenv("WORKER_PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
